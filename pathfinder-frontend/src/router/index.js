42cf46b8e32ce8741ac694ac9ad2bcf40843fe12
UNKNOWNAR <amiarinjaysarkar@gmail.com>
2026-02-26 23:47:03 +0530

    fix: switch to hash router for GitHub Pages compatibility

--- pathfinder-frontend/src/router/index.js
+++ pathfinder-frontend/src/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import LoginView from '../views/LoginView.vue';
 
 const routes = [
-  { path: '/', redirect: '/login' },
+  { path: '/', redirect: '/dashboard' },
   {
     path: '/login',
     name: 'login',
@@ -32,7 +32,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
 });
 
