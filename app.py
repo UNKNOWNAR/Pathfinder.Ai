4026c418b424f969ce194b7c9ffcf00db9c930b2
UNKNOWNAR <amiarinjaysarkar@gmail.com>
2026-02-28 13:02:52 +0530

    refactor: modularize API registration via init_routes()
    Moved all api.add_resource() calls out of app.py into a new
    api/__init__.py with an init_routes(api) function. app.py now
    calls init_routes(api) to register all endpoints cleanly.

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from flask import Flask
 from models import db
 from models.user import User
-from routes.generate_resume import resume_bp  # Import your new blueprint
 from config import Config
 from flask_security import Security
 from flask_restful import Api
 from user_datastore import user_datastore
-from api.auth_apis import LoginUser, SignUpUser,LogoutUser
-from api.profile_api import ProfileAPI
-from api.generate_resume import GenerateResume
+from api import init_routes
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 
@@ -21,9 +18,7 @@ def create_app():
     JWTManager(app)
 
     api = Api(app)
-    app.register_blueprint(resume_bp)
-
-    return app,api
+    return app, api
 
 def init_db():
     with app.app_context():
@@ -37,15 +32,11 @@ def init_db():
             )
             db.session.commit()
 
-app,api = create_app()
+app, api = create_app()
 CORS(app)
 init_db()
-api.add_resource(LoginUser, '/login')
-api.add_resource(SignUpUser, '/signup')
-api.add_resource(LogoutUser, '/logout')
-api.add_resource(ProfileAPI, '/profile')
-api.add_resource(GenerateResume, '/generate-resume')
+init_routes(api)
 
-if __name__ == '__main__':  
+if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    app.run(debug=app.config.get('DEBUG', False))
