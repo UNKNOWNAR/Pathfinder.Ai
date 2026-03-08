6d028ade266409558345dcbbe2fe41a42e9584e8
UNKNOWNAR <amiarinjaysarkar@gmail.com>
2026-03-08 16:44:32 +0530

    fix(frontend): set production api baseURL for Amplify deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.VUE_APP_API_URL || 'http://127.0.0.1:5000'
+    baseURL: process.env.VUE_APP_API_URL || 'https://api.pathfinderai.me'
 });
 
 api.interceptors.request.use(config => {
